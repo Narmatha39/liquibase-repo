ddc25adcf53fc6966e230a2a6f33584a99817445
Narmatha39 <152671316+Narmatha39@users.noreply.github.com>
2024-05-15 18:18:28 +0530

    Update example-changelog.sql

--- example-changelog.sql
+++ example-changelog.sql
@@ -1,26 +1,8 @@
 --liquibase formatted sql
 --changeset nvoxland:1
-create table test1 (
+create table test10 (
     id int primary key,
     name varchar(255)
 );
---rollback drop table test1;
---changeset nvoxland:7
-create table test7 (
-    id int primary key,
-    name varchar(255)
-);
---rollback drop table test7;
---changeset nvoxland:8
-create table test8 (
-    id int primary key,
-    name varchar(255)
-);
---rollback drop table test8;
---changeset nvoxland:9
-create table test9 (
-    id int primary key,
-    name varchar(255)
-);
---rollback drop table test9;
+--rollback drop table test10;
 
